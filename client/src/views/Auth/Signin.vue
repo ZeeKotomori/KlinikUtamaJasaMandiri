4f64207e6b491ab4efbab236c11836027b2ffe2f
Virz <virgiriyadi2404@gmail.com>
2025-04-24 15:10:13 +0700

    refactor: update Signin view text to English and improve layout structure

--- client/src/views/Auth/Signin.vue
+++ client/src/views/Auth/Signin.vue
@@ -11,10 +11,10 @@
                 <h1
                   class="mb-2 font-semibold text-gray-800 text-title-sm dark:text-white/90 sm:text-title-md"
                 >
-                  Masuk
+                  Login
                 </h1>
                 <p class="text-sm text-gray-500 dark:text-gray-400">
-                  Masukkan email dan password untuk masuk!
+                  Enter your email and password to Login!
                 </p>
               </div>
               <div>
@@ -33,7 +33,7 @@
                         type="email"
                         id="email"
                         name="email"
-                        placeholder="dokter@gmail.com"
+                        placeholder="doctorJohn@gmail.com"
                         class="dark:bg-dark-900 h-11 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800"
                       />
                     </div>
@@ -50,7 +50,7 @@
                           v-model="password"
                           :type="showPassword ? 'text' : 'password'"
                           id="password"
-                          placeholder="Masukkan password anda"
+                          placeholder="Enter your password"
                           class="dark:bg-dark-900 h-11 w-full rounded-lg border border-gray-300 bg-transparent py-2.5 pl-4 pr-11 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800"
                         />
                         <span
@@ -97,7 +97,7 @@
                       <router-link
                         to="/reset-password"
                         class="text-sm text-brand-500 hover:text-brand-600 dark:text-brand-400"
-                        >Lupa password?</router-link
+                        >Forgot password?</router-link
                       >
                     </div>
                     <!-- Button -->
@@ -106,7 +106,7 @@
                         type="submit"
                         class="flex items-center justify-center w-full px-4 py-3 text-sm font-medium text-white transition rounded-lg bg-brand-500 shadow-theme-xs hover:bg-brand-600"
                       >
-                        Sign In
+                        Login
                       </button>
                     </div>
                   </div>
