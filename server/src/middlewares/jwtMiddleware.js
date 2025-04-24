eb007773596fc2fdc8a7188da20bae620302ba21
ZeeKotomori <zikriaf23@gmail.com>
2025-04-24 13:37:25 +0700

    feat: add JWT middleware for token authentication

---
+++ server/src/middlewares/jwtMiddleware.js
@@ -0,0 +1,17 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const { JWT_SECRET } = process.env;
+
+export const authenticateToken = (req, res, next) => {
+    const token = req.headers['authorization']?.split(' ')[1];
+    if (!token) return res.status(401).json({ error: 'Access denied. No token provided.' });
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        return res.status(403).json({ error: 'Invalid token' });
+    }
+};
